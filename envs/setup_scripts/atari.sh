16536c8f9fcdb0df4fb36f0e17a6db2d92ba8414
scott <scottie@Scotts-MacBook-Pro-2.local>
2025-05-15 14:01:30 +0100

    atari setup

--- envs/setup_scripts/atari.sh
+++ envs/setup_scripts/atari.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Run this script to install Atari
-pip3 install atari-py==0.2.9
-pip3 install opencv-python==4.7.0.72
+pip install atari-py==0.2.9
+pip install opencv-python==4.7.0.72
 mkdir roms && cd roms
 wget -L -nv http://www.atarimania.com/roms/Roms.rar
-unrar x -o+ Roms.rar
-python3 -m atari_py.import_roms ROMS
+7z x -aoa Roms.rar
+python -m atari_py.import_roms ROMS
 cd .. && rm -rf roms
