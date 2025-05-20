40646784e1d86521e6370e9b269a32deff2d9185
Scott Jones <scottjoneswilliam@gmail.com>
2025-05-20 17:50:02 +0100

    fix timelimit wrapper to handle multiple envs

--- envs/wrappers.py
+++ envs/wrappers.py
@@ -11,13 +11,14 @@ class TimeLimit(gym.Wrapper):
         self._step = None
 
     def step(self, action):
+
         assert self._step is not None, "Must reset environment."
         obs, reward, done, info = self.env.step(action)
         self._step += 1
         if self._step >= self._duration:
-            done = True
+            done = np.ones(self.env._num_envs, dtype=bool) if hasattr(self.env, "_num_envs") else True
             if "discount" not in info:
-                info["discount"] = np.array(1.0).astype(np.float32)
+                info["discount"] = np.ones(self.env._num_envs).astype(np.float32) if hasattr(self.env, "_num_envs") else np.array(1.0).astype(np.float32)
             self._step = None
         return obs, reward, done, info
 
